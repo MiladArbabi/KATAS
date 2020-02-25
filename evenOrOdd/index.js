55be945589440e5494b282dd3e58ba80d16393f3
Milad <milad@Milads-MacBook-Pro.local>
2020-02-25 19:58:34 +0100

    Morsecode using switch long one - String manipulation

---
+++ evenOrOdd/index.js
@@ -0,0 +1,10 @@
+function even_or_odd(number) {
+    return number % 2 ? "Odd" : "Even"
+  }
+
+console.log(even_or_odd(9));
+console.log(even_or_odd(92));
+console.log(even_or_odd(19));
+console.log(even_or_odd(970));
+
+
